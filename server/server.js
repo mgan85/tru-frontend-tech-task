2f6c82ec8efb48ac2039f150675d0fb57090df83
Giorgia <Giorgia.amici@trurating.com>
2018-06-14 14:55:40 +0100

    Updating Readme, mocks and server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ app.get('/measures', (req, res) => {
     const shoppingChannel = req.query.shoppingChannel;
     const randomTimer = Math.round(Math.random() * (3000 - 500)) + 500;
         if (!shoppingChannel) {
-            throw new Error('something bad happened! Check you passed all the needed query params');
+            res.status(400)        // HTTP status 404: Bad Request
+                .send('Bad Request ');
         }
         if (shoppingChannel == 'instore') {
             setTimeout(function () {
