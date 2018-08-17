2b8b9183479103bd79c994adea655b93226660cd
mgan85 <mgan@o2.pl>
2018-08-17 15:47:16 +0100

    draft IconBar component

---
+++ client/src/IconBar/IconBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./IconBar.css;
+
+/*
+ * Stateless component which show bar with icon
+ * props.icons - array of icons to show
+ * props.color - background color for bar
+ */
+const IconBar = (props) => {
+
+
+}
+
+export default IconBar;
