fac1b57b048509b656104352f1852bb88b751a19
mgan85 <mgan@o2.pl>
2018-08-17 15:47:39 +0100

    draft IconElement component

---
+++ client/src/IconBar/IconElement/IconElement.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./IconElement.css;
+
+/*
+ * Stateless component represent icon
+ * props.url - url for icon
+ * props.label - optional text under icon
+ * props.position - icon position (left/ centre/ right)
+ * props.padding - optional padding for icon
+ */
+const IconElement = (props) => {
+
+
+}
+
+export default IconElement;
